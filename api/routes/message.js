4e2a5a699d64612f416ef905c9b97d5ff7f59f02
jaicabher1 <jaicabher1@alum.us.es>
2025-03-14 17:43:47 +0100

    fix: show messages

--- api/routes/message.js
+++ api/routes/message.js
@@ -5,9 +5,9 @@ var MessageController = require('../controllers/message');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
-api.post('/message', md_auth.ensureAuth, MessageController.sendMessage);
-api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
-api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getEmittedMessages);
+api.post('/send-message', md_auth.ensureAuth, MessageController.sendMessage);
+api.get('/received-messages', md_auth.ensureAuth, MessageController.getReceivedMessages);
+api.get('/my-messages', md_auth.ensureAuth, MessageController.getEmittedMessages);
 api.get('/unviewed-messages', md_auth.ensureAuth, MessageController.getUnviewedMessages);
 api.get('/set-viewed-messages', md_auth.ensureAuth, MessageController.setViewedMessages);
 
